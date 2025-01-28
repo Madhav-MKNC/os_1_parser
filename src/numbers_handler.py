7512364f0b07dc61aab3caa0d3e4129148fbaf35
Madhav <Madhavampire@gmail.com>
2025-01-28 18:59:12 +0530

    `

--- src/numbers_handler.py
+++ src/numbers_handler.py
@@ -1,45 +1,48 @@
 # Extract, collapse and pad phone numbers and pincode
 
 import re
+# from src.phone_number_lookup import PhoneNumberLookup
+# from src.phonenumber import PhoneNumber
+# from src.pincode import PinCode
+
+
+# phone_number_lookup = PhoneNumberLookup()
+# phone_number = PhoneNumber(phone_number_lookup)
+# pincode = PinCode()
 
 
 class NumbersHandler:
     def __init__(self):
         self.expected_chars = [" ", "-", "_"]
-        self.typo_chars = {"i": 1, "I": 1, "o": 0, "O": 0}
+        self.typo_chars = {"i": "1", "I": "1", "o": "0", "O": "0"}
 
-    def is_valid_phone_number_or_valid_pin(self, inp):
-        try:
-            if len(str(int(inp))) == 10 or len(inp) == 6:
-                return True
-        except:
-            pass 
-        return False
+    def fix_digit_typos(self, text):
+        # Rule: split texts around space
+        # iff a group contains atleast one 0-9 and only typo chars
+        # group has atlest 3 chars
 
-    def this_is_a_typo(self, text, index, reverse_direction=None):
-        # rule: should be attached with some valid digit (0-9) or (o/i) iff those are attached with some valid digits too
-        # print(text, index)
+        splits = text.split(" ")
+        new_splits = []
+        for grp in splits:
+            if len(grp) >= 3:
+                # Check if the string contains at least one digit and only 'i', 'o', and digits (no other characters)
+                if re.match(r'^[io0-9]*$', grp) and re.search(r'\d', grp):
+                # if re.match(r'^[io0-9]*$', grp) and re.search(r'\d', grp): # add self.expected_chars in the check
+                    for char in self.typo_chars:
+                        grp = grp.replace(char, self.typo_chars[char])
+            new_splits.append(grp)
 
-        if reverse_direction is None or reverse_direction == False:
-            if index < len(text) - 1:
-                if text[index+1].isdigit(): return True 
-                if text[index+1] in self.expected_chars:
-                    if text[index-1] in self.expected_chars: return False
-                    return self.this_is_a_typo(text, index+1, reverse_direction=False)
-                if text[index+1] in self.typo_chars: return self.this_is_a_typo(text, index+1, reverse_direction=False)
+        text = " ".join(new_splits)
+        return text
 
-        if reverse_direction is None or reverse_direction == True:
-            if index > 0:
-                if text[index-1].isdigit(): return True
-                if text[index-1] in self.expected_chars:
-                    if text[index+1] in self.expected_chars: return False
-                    return self.this_is_a_typo(text, index-1, reverse_direction=True)
-                if text[index-1] in self.typo_chars: return self.this_is_a_typo(text, index-1, reverse_direction=True)
-
-        return False
 
     def pad_numbers(self, address_string, address_obj):
+        # address_string = phone_number.collapse_phone_number_and_pin(address_string)
+        # address_string = phone_number.pad_phone_number(address_string, "*", address_obj)
+        # address_string = pincode.pad_pin_code(address_string, "*", address_obj)
         # return address_string
+
+        address_string = self.fix_digit_typos(address_string)
         new_text = address_string
         pad_char = "*"
 
@@ -63,13 +66,6 @@ class NumbersHandler:
             elif char in self.expected_chars:
                 temp_text = padded_text[:i] + padded_text[i+1:] # skip/remove this char
                 continue
-            elif char in self.typo_chars and self.this_is_a_typo(padded_text, i):
-                char = self.typo_chars[char]
-                number_str += char
-                number_str = str(int(number_str)) # For removing preceding 0's
-                if digit_begin:
-                    temp_text = padded_text[:i] + pad_char + padded_text[i:] # beginning pad_char
-                    digit_begin = False
             else:
                 if number_str:
                     if len(number_str) == 10:
@@ -79,13 +75,13 @@ class NumbersHandler:
                     elif len(number_str) == 12 and number_str[:2] == "91":
                         if len(str(int(number_str[2:]))) == 10:
                             phone_nums.append(number_str[2:])
-                            temp_text = padded_text[:i] + pad_char + padded_text[i:] # ending pad_char
+                            temp_text = padded_text[:i-1] + pad_char + padded_text[i-1:] # ending pad_char
                             new_text = temp_text
                         else: faulty_nums.append(number_str)
                     elif len(number_str) == 6:
                         if not pin_code:
                             pin_code = number_str
-                            temp_text = padded_text[:i] + pad_char + padded_text[i:] # ending pad_char
+                            temp_text = padded_text[:i-1] + pad_char + padded_text[i-1:] # ending pad_char
                             temp_text = new_text
                         else: # i.e. entry with more than 1 pincode -> faulty
                             faulty_nums.append(number_str) # so faulty_nums might also have len 6 nums
@@ -106,13 +102,20 @@ class NumbersHandler:
             if len(i) in [6, 8, 9, 11] or len(i) > 12:
                 address_obj.faulty = "FAULTY"
                 break
-
+        print(new_text)
         return new_text
 
 
 if __name__ == "__main__":
     x = NumbersHandler()
-    print(x.this_is_a_typo('iiii-oooi1', 2))
-    print(x.this_is_a_typo('iiiiooo i 1', 2))
-    print(x.this_is_a_typo('iiiirooo i 1', 2))
-
+    tests = """
+iiii-oooi1      2
+iiiiooo i 1     2 
+iiiirooo i 1    2
+bii5
+    """.strip()
+    for i in tests.split('\n'):
+        if 'off' in i: continue
+        i = i.strip()
+        inp, index = i[:-2], int(i[-1])-1 # only one digit indexes
+        print(x.valid_digit(inp, index))
